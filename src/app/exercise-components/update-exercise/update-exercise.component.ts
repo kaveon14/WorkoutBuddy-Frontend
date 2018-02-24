a1b1e087667336d0c7162fa3ae280ec7eb9528e6
Kaveon Smith <kaveon31@gmail.com>
2018-02-24 13:22:35 -0600

    created for updating or deleting custom exercises

--- src/app/exercise-components/update-exercise/update-exercise.component.ts
+++ src/app/exercise-components/update-exercise/update-exercise.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import {Exercise} from "../models/exercise";
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Exercise} from "../../models/exercise";
 import {FormBuilder, FormGroup} from "@angular/forms";
-import {ExerciseService} from "../exercise-services/exercise.service";
+import {ExerciseService} from "../../exercise-services/exercise.service";
 import {ActivatedRoute} from "@angular/router";
-// need to check if any changes were made
+
 @Component({
   selector: 'app-update-exercise',
   templateUrl: './update-exercise.component.html',
@@ -13,35 +13,80 @@ export class UpdateExerciseComponent implements OnInit {
 
   exercise: Exercise;
   formG: FormGroup;
+  @ViewChild("file") file;
 
   constructor(private fb: FormBuilder,private exerciseService: ExerciseService,
               private route: ActivatedRoute) {
-
+    this.exercise = new Exercise();
   }
 
   ngOnInit() {
     this.getCustomExercise();
-    console.log(this.exercise.exercise_name);
-    //this.createForm();
+    this.createForm();
   }
 
   createForm() {
     this.formG = this.fb.group({
-      exercise_name: this.exercise.exercise_name,
-      exercise_description: this.exercise.exercise_description,
-      exercise_image: this.exercise.exercise_image
+      exercise_nam: this.exercise.exercise_name as string,
+      exercise_description: this.exercise.exercise_description as string,
     });
   }
 
   getCustomExercise() {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.exerciseService.getExercise(id,true).subscribe(ex => this.exercise = ex['RequestResponse']);
+    this.exerciseService.getExercise(id,false).subscribe(ex => {
+      this.exercise = ex['RequestResponse'];
+      this.createForm();
+    });
   }
 
-  updateExercise() {
-    //do shit
+  updateExercise() {//called twice for some retarded reason
+    const model = this.formG.value;
+    let exercise = new Exercise();
+    exercise.id = this.exercise.id;
+    exercise.exercise_name = model.exercise_nam as string;
+    exercise.exercise_description = model.exercise_description as string;
+    exercise.profileId = 1;
+    this.exercise.profileId = 1;//just for testing purposes
+    this.exerciseService.updateCustomExercise(exercise).subscribe(res => {
+      console.log(res)});
+    this.updateImageFile(exercise);
   }
 
+  deleteExercise() {
+    this.exercise.profileId = 1;
+    this.exerciseService.deleteCustomExercise(this.exercise).subscribe(res => {
+      console.log(res)});
+  }
 
+  updateImgSrc() {
+    let file = this.getImageFile();
+    let fileReader = new FileReader();
+    fileReader.readAsDataURL(file as File);
+    var preview = document.querySelector('img');
+
+    fileReader.addEventListener("load",  function() {
+      preview.src = fileReader.result;
+    });
+  }
+
+  updateImageFile(exercise: Exercise): void {
+    let fileToUpload = this.getImageFile();
+    if(fileToUpload != null) {
+      this.exerciseService
+        .updateCustomExerciseImage(fileToUpload,exercise.id)
+        .subscribe(res => {
+          console.log(res);
+        });
+    }
+  }
+
+  getImageFile(): File {
+    let fi = this.file.nativeElement;
+    if (fi.files && fi.files[0]) {
+      return fi.files[0];
+    }
+    return null;
+  }
 
 }
