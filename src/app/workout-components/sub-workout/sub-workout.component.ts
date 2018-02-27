5c4e1c3ea991547f9fd4c71a9ee506d5fdf67642
Kaveon Smith <kaveon31@gmail.com>
2018-02-26 21:51:31 -0600

    added code to show subworkouts and their exercise goals

--- src/app/workout-components/sub-workout/sub-workout.component.ts
+++ src/app/workout-components/sub-workout/sub-workout.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {SubWorkout} from "../../models/sub-workout";
+import {WorkoutService} from "../../workout-service/workout.service";
+import {ActivatedRoute} from "@angular/router";
+import {ExerciseGoals} from "../../models/exercise-goals";
+import {componentRefresh} from "@angular/core/src/render3/instructions";
+import {falseIfMissing} from "protractor/built/util";
 
 @Component({
   selector: 'app-sub-workout',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubWorkoutComponent implements OnInit {
 
-  constructor() { }
+  sub_workout_list: SubWorkout[];
+  exercise_goals: ExerciseGoals[];
+
+  d: boolean;
+
+  constructor(private route: ActivatedRoute,private workoutService: WorkoutService) {
+    this.sub_workout_list = [];
+    this.exercise_goals = [];
+    this.d = true;
+  }
 
   ngOnInit() {
+    this.getSubWorkouts()
+  }
+
+  getSubWorkouts() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.workoutService.getSubWorkouts(id).subscribe(r =>
+      this.sub_workout_list = r['RequestResponse']);
+  }
+
+  getExerciseGoals(id: number) {
+    this.workoutService.getExerciseGoals(id).subscribe(r => {
+      this.exercise_goals = r['RequestResponse'];
+      console.log(r['RequestResponse']);
+  }
+    );
+    this.d = false;
+  }
+
+  flip() {
+    this.d = true;
   }
 
 }
