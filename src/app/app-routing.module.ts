65609638449ef4f267d1d7b36287abe2fd0ed090
Kaveon Smith <kaveon31@gmail.com>
2018-02-26 21:56:14 -0600

    added new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from "@angular/router";
-import {ExercisesComponent} from "./exercises/exercises.component";
-import {ExerciseDetailComponent} from "./exercise-detail/exercise-detail.component";
+import {ExercisesComponent} from "./exercise-components/exercises/exercises.component";
+import {ExerciseDetailComponent} from "./exercise-components/exercise-detail/exercise-detail.component";
+import {CreateExerciseComponent} from "./exercise-components/create-exercise/create-exercise.component";
+import {UpdateExerciseComponent} from "./exercise-components/update-exercise/update-exercise.component";
+import {UserLoginComponent} from "./user-profile-components/user-login/user-login.component";
+import {MainWorkoutComponent} from "./workout-components/main-workout/main-workout.component";
+import {SubWorkoutComponent} from "./workout-components/sub-workout/sub-workout.component";
 
 const routes = [
   {path: 'exercises',component: ExercisesComponent},
-  {path: 'exercise/:id',component: ExerciseDetailComponent}
+  {path: 'exercise/:id/:default_exercise',component: ExerciseDetailComponent},
+  {path: 'createExercise',component: CreateExerciseComponent},
+  {path: 'updateExercise/:id', component: UpdateExerciseComponent},
+  {path:'login', component: UserLoginComponent},
+  {path:'mainworkouts', component: MainWorkoutComponent},
+  {path:'subworkouts/:id', component: SubWorkoutComponent},
 ]
 
 @NgModule({
