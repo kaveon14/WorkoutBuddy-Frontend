72050333c8ad87316dc62f3b5c8d23a34fa57656
Kaveon Smith <kaveon31@gmail.com>
2018-02-26 21:53:21 -0600

    added to handle workout requests, added function to get mainworkouts,subworkouts,and exercise goals

---
+++ src/app/workout-service/workout.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {MainWorkout} from "../models/main-workout";
+import {catchError} from "rxjs/operators";
+import {ProfileService} from "../user-profile-service/profile.service";
+import {HttpClient} from "@angular/common/http";
+import {of} from "rxjs/observable/of";
+import {SubWorkout} from "../models/sub-workout";
+import {ExerciseGoals} from "../models/exercise-goals";
+
+@Injectable()
+export class WorkoutService {
+
+  constructor(private http: HttpClient,private profileService: ProfileService) { }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  getMainWorkouts(): Observable<MainWorkout[]> {
+    const url = 'http://127.0.0.1:8000/testWorkoutApi/getMainWorkouts';
+    let input = new FormData();
+    input.append('profileId',this.profileService.profileId.toString());
+    return this.http.post<MainWorkout[]>(url,input).pipe(
+      catchError(this.handleError('getMainWorkouts', []))
+    );
+  }
+
+  getSubWorkouts(id: number): Observable<SubWorkout[]>{
+    const url = 'http://127.0.0.1:8000/testWorkoutApi/getSubWorkouts';
+    let input = new FormData();
+    input.append('mainWorkoutId',id.toString());
+    return this.http.post<SubWorkout[]>(url,input).pipe(
+      catchError(this.handleError('getSubWorkouts', []))
+    );
+  }
+
+  getExerciseGoals(id: number):Observable<ExerciseGoals[]> {
+    const url = `http://127.0.0.1:8000/testWorkoutApi/getSubWorkoutExercises/?subWorkoutId=${id}`;
+    //let input = new FormData();
+    //input.append('subWorkoutId',id.toString());
+    return this.http.get<ExerciseGoals[]>(url).pipe(
+      catchError(this.handleError('getSubWorkoutExercises', []))
+    );
+  }
+
+}
