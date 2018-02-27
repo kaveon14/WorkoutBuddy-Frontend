a3693bd5670ac770deb09bc4bc4b2f97d93246cb
Kaveon Smith <kaveon31@gmail.com>
2018-02-26 21:51:13 -0600

    added code to show mainworkouts

--- src/app/workout-components/main-workout/main-workout.component.ts
+++ src/app/workout-components/main-workout/main-workout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MainWorkout} from "../../models/main-workout";
+import {WorkoutService} from "../../workout-service/workout.service";
 
 @Component({
   selector: 'app-main-workout',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainWorkoutComponent implements OnInit {
 
-  constructor() { }
+
+  main_workout_list: MainWorkout[];
+
+  constructor(private workoutService: WorkoutService) {
+    this.main_workout_list = [];
+  }
 
   ngOnInit() {
+    this.getMainWorkouts();
+  }
+
+  getMainWorkouts() {
+    this.workoutService.getMainWorkouts().subscribe(main_workouts =>
+      this.main_workout_list = main_workouts['RequestResponse']);
   }
 
 }
