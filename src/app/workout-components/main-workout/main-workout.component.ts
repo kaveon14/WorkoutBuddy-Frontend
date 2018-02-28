84bdb55610fdfdb42437bea1661f5df1dfabe488
Kaveon Smith <kaveon31@gmail.com>
2018-02-27 22:37:40 -0600

    added methods to create new mainWorkouts

--- src/app/workout-components/main-workout/main-workout.component.ts
+++ src/app/workout-components/main-workout/main-workout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MainWorkout} from "../../models/main-workout";
 import {WorkoutService} from "../../workout-service/workout.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-main-workout',
@@ -9,11 +10,14 @@ import {WorkoutService} from "../../workout-service/workout.service";
 })
 export class MainWorkoutComponent implements OnInit {
 
-
   main_workout_list: MainWorkout[];
+  formG: FormGroup;
+  hide_form: boolean;
 
-  constructor(private workoutService: WorkoutService) {
+  constructor(private fb: FormBuilder,private workoutService: WorkoutService) {
     this.main_workout_list = [];
+    this.hide_form = true;
+    this.createForm();
   }
 
   ngOnInit() {
@@ -23,6 +27,27 @@ export class MainWorkoutComponent implements OnInit {
   getMainWorkouts() {
     this.workoutService.getMainWorkouts().subscribe(main_workouts =>
       this.main_workout_list = main_workouts['RequestResponse']);
+  }
+
+  createForm() {
+    this.formG = this.fb.group({
+      main_workout_name:'',
+    });
+  }
+
+  showMWForm() {
+    this.hide_form = false;
+  }
+
+  createMainWorkout() {
+    const model = this.formG.value;
+    let mainWorkout = new MainWorkout();
+    mainWorkout.main_workout_name = model.main_workout_name as string;
+    this.workoutService.createMainWorkout(mainWorkout).subscribe(res => {
+      mainWorkout.id = res['RequestResponse']['id'];
+      this.main_workout_list.push(mainWorkout);
+    });
+    this.hide_form = true;
   }
 
 }
