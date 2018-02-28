af65b45786e8275d76aa49807d7a3e6d54594be4
Kaveon Smith <kaveon31@gmail.com>
2018-02-27 22:39:05 -0600

    updated main/sub workout models to include more id's

--- src/app/models/main-workout.ts
+++ src/app/models/main-workout.ts
@@ -1,4 +1,5 @@
 export class MainWorkout {
   id: number;
+  profileId: number;
   main_workout_name: string;
 }
--- src/app/models/sub-workout.ts
+++ src/app/models/sub-workout.ts
@@ -1,4 +1,9 @@
+import {Exercise} from "./exercise";
+
 export class SubWorkout {
   id: number;
+  profileId: number;
+  mainWorkoutId: number;
   sub_workout_name: string;
+  exercise_list: Exercise[];
 }
