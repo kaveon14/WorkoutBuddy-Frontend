fa8f858b0923489259a7eb10ed4f678feaa788b9
Kaveon Smith <kaveon31@gmail.com>
2018-02-28 14:20:11 -0600

    cretaed to allow user to logout

--- src/app/user-profile-components/user-login/user-login.component.ts
+++ src/app/user-profile-components/user-login/user-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {ProfileService} from "../../user-profile-service/profile.service";
 
 @Component({
   selector: 'app-user-login',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginComponent implements OnInit {
 
-  constructor() { }
+  formG: FormGroup;
+
+  constructor(private fb: FormBuilder,private profileService: ProfileService) {
+    this.createForm()
+  }
 
   ngOnInit() {
+  }
+
+  createForm() {
+    this.formG = this.fb.group( {
+      username: '',
+      password: '',
+    })
+  }
+
+  login() {
+    const model = this.formG.value;
+    let username = model.username;
+    let password = model.password;
+    this.profileService.login(username,password).subscribe(r => {
+      console.log(this.profileService.profileId = r['RequestResponse']['profileId']);
+    })
   }
 
 }
