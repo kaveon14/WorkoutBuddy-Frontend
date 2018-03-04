e8e8c8668fcbf4f810db0ac96749d5880f36b462
Kaveon Smith <kaveon31@gmail.com>
2018-03-03 23:40:39 -0600

    added new methods for updating and deleting main/sub workouts

--- src/app/workout-service/workout.service.ts
+++ src/app/workout-service/workout.service.ts
@@ -6,7 +6,7 @@ import {ProfileService} from "../user-profile-service/profile.service";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {of} from "rxjs/observable/of";
 import {SubWorkout} from "../models/sub-workout";
-import {ExerciseGoals} from "../models/exercise-goals";
+import {ExerciseGoal} from "../models/exercise-goals";
 
 const postOptions = {
   headers: new HttpHeaders({'Content-Type':'application/x-www-form-urlencoded'},)
@@ -49,11 +49,11 @@ export class WorkoutService {
     );
   }
 
-  getExerciseGoals(id: number):Observable<ExerciseGoals[]> {
+  getExerciseGoals(id: number):Observable<ExerciseGoal[]> {
     const url = `http://127.0.0.1:8000/testWorkoutApi/getSubWorkoutExercises/?subWorkoutId=${id}`;
     //let input = new FormData();
     //input.append('subWorkoutId',id.toString());
-    return this.http.get<ExerciseGoals[]>(url).pipe(
+    return this.http.get<ExerciseGoal[]>(url).pipe(
       catchError(this.handleError('getSubWorkoutExercises', []))
     );
   }
@@ -68,6 +68,45 @@ export class WorkoutService {
     const url = 'http://127.0.0.1:8000/testWorkoutApi/createSubWorkout';
     subWorkout.profileId = this.profileService.profileId;
     return this.http.post<SubWorkout>(url,subWorkout,postOptions);
+  }
+
+  updateMainWorkoutName(mainWorkout:MainWorkout): Observable<MainWorkout> {
+    const url = 'http://127.0.0.1:8000/testWorkoutApi/updateMainWorkoutName';
+    return this.http.post<MainWorkout>(url,mainWorkout,postOptions);
+  }
+
+  updateSubWorkoutName(subWorkout:SubWorkout): Observable<SubWorkout> {
+    const url ='http://127.0.0.1:8000/testWorkoutApi/updateSubWorkoutName';
+    return this.http.post<SubWorkout>(url,subWorkout,postOptions);
+  }
+
+  deleteMainWorkout(mainWorkout:MainWorkout) {
+    const url = 'http://127.0.0.1:8000/testWorkoutApi/deleteMainWorkout';
+    mainWorkout.profileId = this.profileService.profileId;
+    return this.http.post<MainWorkout>(url,mainWorkout,postOptions);
+  }
+
+  deleteSubWorkout(subWorkout:SubWorkout) {
+    const url = 'http://127.0.0.1:8000/testWorkoutApi/deleteSubWorkout';
+    return this.http.post<SubWorkout>(url,subWorkout,postOptions);
+  }
+
+  updateExerciseGoals(subWorkout:SubWorkout): Observable<SubWorkout> {
+    const url = 'http://127.0.0.1:8000/testWorkoutApi/updateSubWorkoutExerciseGoals';
+    return this.http.post<SubWorkout>(url,subWorkout,postOptions);
+  }
+
+  addExerciseGoals(subWorkout:SubWorkout): Observable<SubWorkout> {
+    const url = 'http://127.0.0.1:8000/testWorkoutApi/addExerciseGoals';
+    return this.http.post<SubWorkout>(url,subWorkout,postOptions);
+  }
+
+  deleteExerciseGoals(subWorkout:SubWorkout,ex_goal:ExerciseGoal): Observable<SubWorkout> {
+    const url = 'http://127.0.0.1:8000/testWorkoutApi/deleteExerciseGoals';
+    let input = new FormData();
+    input.append('subWorkoutId',subWorkout.id.toString());
+    input.append('exercise_goal_id',ex_goal.id.toString());
+    return this.http.post<SubWorkout>(url,input);
   }
 
 }
