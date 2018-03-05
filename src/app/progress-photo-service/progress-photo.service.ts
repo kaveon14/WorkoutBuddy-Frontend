7e40cd3442678ba06d7a2dccff566d8650fd76f2
Kaveon Smith <kaveon31@gmail.com>
2018-03-05 15:02:38 -0600

    added new method for creating progress photos

--- src/app/progress-photo-service/progress-photo.service.ts
+++ src/app/progress-photo-service/progress-photo.service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {ProfileService} from "../user-profile-service/profile.service";
+import {Observable} from "rxjs/Observable";
+import {ProgressPhoto} from "../models/progress-photo";
+
+const t = {
+  headers: new HttpHeaders( {
+    'X-CSRFToken':'csrftoken'
+  })
+};
+
+const postOptions = {
+  headers: new HttpHeaders({'Content-Type':'application/x-www-form-urlencoded'},)
+};
 
 @Injectable()
 export class ProgressPhotoService {
 
-  constructor() { }
+  constructor(private http: HttpClient,private profileService: ProfileService) { }
+
+  getProgressPhotos(): Observable<ProgressPhoto[]> {
+    const url = 'http://127.0.0.1:8000/testProgressPhotoApi/getProgressPhotos';
+    let input = new FormData();
+    input.append('profileId','1');
+    return this.http.post<ProgressPhoto[]>(url,input,t);
+  }
+
+  addProgressPhoto(file:any) {
+    const url = 'http://127.0.0.1:8000/testProgressPhotoApi/addProgressPhotos';
+    let input = new FormData();
+    input.append("file", file);
+    input.append("profileId",this.profileService.profileId.toString());
+    return this.http.post(url,input);
+  }
+
+  deleteProgressPhoto(photo:ProgressPhoto) {
+    const url = 'http://127.0.0.1:8000/testProgressPhotoApi/deleteProgressPhoto';
+    photo.profileId = this.profileService.profileId;
+    return this.http.post<ProgressPhoto[]>(url,photo,postOptions);
+  }
 
 }
