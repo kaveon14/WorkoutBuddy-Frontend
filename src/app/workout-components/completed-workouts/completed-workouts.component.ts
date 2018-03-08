dd295bb01e6de593d9752364a4a2138d5d4061b3
Kaveon Smith <kaveon31@gmail.com>
2018-03-07 21:56:10 -0600

    added methods to view completed workouts

--- src/app/workout-components/completed-workouts/completed-workouts.component.ts
+++ src/app/workout-components/completed-workouts/completed-workouts.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Workout} from "../../models/workout";
+import {WorkoutService} from "../../workout-service/workout.service";
+import {WorkoutExercise} from "../../models/workout-exercise";
 
 @Component({
   selector: 'app-completed-workouts',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompletedWorkoutsComponent implements OnInit {
 
-  constructor() { }
+  workouts: Workout[];
+  clicked_workout: Workout;
+  ex: WorkoutExercise[];
+  hide_workout: boolean;
+
+  constructor(private workoutService: WorkoutService) {
+    this.hide_workout = true;
+    this.clicked_workout = null;
+    this.ex = [];
+  }
 
   ngOnInit() {
+    this.getCompletedWorkouts();
+  }
+
+  getCompletedWorkouts() {
+    this.workoutService.getCompletedWorkouts().subscribe(res =>
+      this.workouts = res['RequestResponse']);
+  }
+
+  getWorkout(workout: Workout) {
+    this.hide_workout = false;
+    this.clicked_workout = workout;
+    this.ex = workout.completed_exercises;
+  }
+
+  revert() {
+    this.hide_workout = true;
   }
 
 }
