26ae50f47b725cbf44523f6df2bdf495d78d3f09
Kaveon Smith <kaveon31@gmail.com>
2018-03-21 22:30:26 -0500

    added new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {RouterModule} from "@angular/router";
 import {ExercisesComponent} from "./exercise-components/exercises/exercises.component";
 import {ExerciseDetailComponent} from "./exercise-components/exercise-detail/exercise-detail.component";
@@ -12,20 +11,26 @@ import {UserLogoutComponent} from "./user-profile-components/user-logout/user-lo
 import {ProgressPhotosComponent} from "./progress-photo-components/progress-photos/progress-photos.component";
 import {AddProgressPhotoComponent} from "./progress-photo-components/add-progress-photo/add-progress-photo.component";
 import {CompletedWorkoutsComponent} from "./workout-components/completed-workouts/completed-workouts.component";
+import {WelcomePageComponent} from "./welcome-page/welcome-page.component";
+import {SubWorkoutDetailComponent} from "./workout-components/sub-wrokout-detail/sub-workout-detail.component";
+import {ExerciseGoalDetailsComponent} from "./workout-components/exercise-goal-details/exercise-goal-details.component";
 
 const routes = [
   {path: 'exercises',component: ExercisesComponent},
   {path: 'exercise/:id/:default_exercise',component: ExerciseDetailComponent},
   {path: 'createExercise',component: CreateExerciseComponent},
   {path: 'updateExercise/:id', component: UpdateExerciseComponent},
+  {path: 'exerciseGoal/:id', component:ExerciseGoalDetailsComponent},
   {path:'login', component: UserLoginComponent},
   {path:'logout', component: UserLogoutComponent},
   {path:'mainworkouts', component: MainWorkoutComponent},
   {path:'subworkouts/:id', component: SubWorkoutComponent},
+  {path: 'subworkout/:id', component: SubWorkoutDetailComponent},
   {path: 'progressPhotos', component: ProgressPhotosComponent},
   {path: 'addProgressPhoto', component:AddProgressPhotoComponent},
-  {path: 'getCompletedWorkouts',component:CompletedWorkoutsComponent}
-]
+  {path: 'getCompletedWorkouts',component:CompletedWorkoutsComponent},
+  {path: '', component: WelcomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
